084bc3a563cad58114cfac997bb40a3940936ee8
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-21 12:00:57 +0200

    Modify database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db');
 
 app.listen(8000, () => {
     console.log('server on port 8000');
@@ -9,44 +10,39 @@ app.listen(8000, () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
-
 app.get('/testimonials', (req, res) => {
-    res.json(db);
+    res.json(db.testimonials);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-    const testimonial = db.find(testimonial => testimonial.id === parseInt(req.params.id));
+    const testimonial = db.testimonials.find(testimonial => testimonial.id === parseInt(req.params.id));
     res.json(testimonial);
 });
 
 app.get('/testimonials/random', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * db.length);
-    const random = db[randomIndex];
+    const randomIndex = Math.floor(Math.random() * db.testimonials.length);
+    const random = db.testimonials[randomIndex];
     res.json(random);
 });
 
 app.post('/testimonials', (req, res) => {
     const { author, text } = req.body;
     const newTestimonial = { id: uuidv4(), author, text };
-    db.push(newTestimonial);
+    db.testimonials.push(newTestimonial);
     res.json({ message: 'OK' });
 });
 
 app.put('/testimonials/:id', (req, res) => {
     const { author, text } = req.body;
-    const testimonial = db.find(testimonial => testimonial.id === parseInt(req.params.id));
+    const testimonial = db.testimonials.find(testimonial => testimonial.id === parseInt(req.params.id));
     testimonial.author = author;
     testimonial.text = text;
     res.json({ message: 'OK' });
 });
 
 app.delete('/testimonials/:id', (req, res) => {
-    const testimonial = db.find(testimonial => testimonial.id === parseInt(req.params.id));
-    db.splice(testimonial, 1);
+    const testimonial = db.testimonials.find(testimonial => testimonial.id === parseInt(req.params.id));
+    db.testimonials.splice(testimonial, 1);
     res.json({ message: 'OK' });
 });
 
