2a61fdd78c17f1825c016e0f20d06739712a3689
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-21 12:36:07 +0200

    Add new endpoints

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.listen(8000, () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//testimonials
 app.get('/testimonials', (req, res) => {
     res.json(db.testimonials);
 });
@@ -46,6 +47,66 @@ app.delete('/testimonials/:id', (req, res) => {
     res.json({ message: 'OK' });
 });
 
+//concerts
+app.get('/concerts', (req, res) => {
+    res.json(db.concerts);
+});
+app.get('/concerts/:id', (req, res) => {
+    const concert = db.concerts.find(concert => concert.id === parseInt(req.params.id));
+    res.json(concert);
+});
+app.post('/concerts', (req, res) => {
+    const { performer, genre, price, day, image } = req.body;
+    const newConcert = { id: uuidv4(), performer, genre, price, day, image };
+    db.concerts.push(newConcert);
+    res.json({ message: 'OK' });
+});
+app.delete('/concerts/:id', (req, res) => {
+    const concert = db.concerts.find(concert => concert.id === parseInt(req.params.id));
+    db.concerts.splice(concert, 1);
+    res.json({ message: 'OK' });
+});
+app.put('/concerts/:id', (req, res) => {
+    const { performer, genre, price, day, image } = req.body;
+    const concert = db.concerts.find(concert => concert.id === parseInt(req.params.id));
+    concert.performer = performer;
+    concert.genre = genre;
+    concert.price = price;
+    concert.day = day;
+    concert.image = image;
+    res.json({ message: 'OK' });
+});
+
+//seats
+app.get('/seats', (req, res) => {
+    res.json(db.seats);
+});
+app.get('/seats/:id', (req, res) => {
+    const seat = db.seats.find(seat => seat.id === parseInt(req.params.id));
+    res.json(seat);
+});
+app.post('/seats', (req, res) => {
+    const { day, seat, client, email } = req.body;
+    const newSeats = { id: uuidv4(), day, seat, client, email };
+    db.seats.push(newSeats);
+    res.json({ message: 'OK' });
+});
+app.delete('/seats/:id', (req, res) => {
+    const seat = db.seats.find(seat => seat.id === parseInt(req.params.id));
+    db.seats.splice(seat, 1);
+    res.json({ message: 'OK' });
+});
+app.put('/seats/:id', (req, res) => {
+    const { day, seat, client, email } = req.body;
+    const seatSelect = db.seats.find(seat => seat.id === parseInt(req.params.id));
+    seatSelect.day = day;
+    seatSelect.seat = seat;
+    seatSelect.client = client;
+    seatSelect.email = email;
+    res.json({ message: 'OK' });
+});
+
+//wrong adress
 app.use((req, res) => {
     res.status(404).json({ message: '404 not found...' });
 });
