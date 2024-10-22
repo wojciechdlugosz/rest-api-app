29b48602558426a417e98f26c27d9329114e9004
Wojciech Dlugosz <webmaster@styronet.pl>
2024-10-22 13:05:13 +0200

    Modify server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 //import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -15,11 +16,18 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+// Find our app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 //wrong adress
 app.use((req, res) => {
     res.status(404).json({ message: '404 not found...' });
 });
 
-app.listen(8000, () => {
-    console.log('server on port 8000');
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
 });
