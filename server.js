243c948eeab6f48849f63ca3ceb2d65ce8aff4a5
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-06 14:31:48 +0100

    Install mongoose, change server to connect with mongo db

--- server.js
+++ server.js
@@ -2,31 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
-
-const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
-});
-const io = socket(server);
-
-//listen to socket
-
-io.on('connection', (socket) => {
-    console.log('New socket ' + socket.id);
-});
+const mongoose = require('mongoose');
 
 //import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+const app = express();
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use((req, res, next) => {
     req.io = io;
     next();
 });
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
@@ -42,4 +35,24 @@ app.get('*', (req, res) => {
 //wrong adress
 app.use((req, res) => {
     res.status(404).json({ message: '404 not found...' });
+});
+
+// connects our backend code with the database
+mongoose.connect('mongodb://0.0.0.0:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+});
+const io = socket(server);
+
+//listen to socket
+
+io.on('connection', (socket) => {
+    console.log('New socket ' + socket.id);
 });
